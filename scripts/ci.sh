c35b723c635255d95b12ba7e8a905316d0d29e65
Guillaume Dallenne <dallenneguillaume@gmail.com>
2022-11-05 12:31:04 +0100

    add Cargo.toml fmt check

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,6 +3,9 @@
 set -e
 
 cargo fmt --check
+cargo install cargo-sort
+cargo sort -c --check-format
+
 cargo build --all-targets --all-features
 cargo clippy --all-targets --all-features -- -D warnings
 cargo test --all-targets --all-features
