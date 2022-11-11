0017f987eb5d68e9e7ce17fb6b038c708f6db8b5
Guillaume Dallenne <dallenneguillaume@gmail.com>
2022-11-11 12:00:04 +0100

    fix ci action

--- ci/check.sh
+++ ci/check.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-cargo fmt --check
-cargo sort --print
-
 cargo build --all-targets --all-features
 cargo clippy --all-targets --all-features -- -D warnings
 cargo test --all-targets --all-features
