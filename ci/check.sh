82af7d7e95774fefa32e5e1b6d9aead0c56ac6ef
Guillaume Dallenne <dallenneguillaume@gmail.com>
2022-11-26 14:42:33 +0100

    ci: add clippy::pedantic but only as warnings

--- ci/check.sh
+++ ci/check.sh
@@ -4,4 +4,5 @@ set -e
 
 cargo build --all-targets --all-features
 cargo clippy --all-targets --all-features -- -D warnings
+cargo clippy --all-targets --all-features -- -W clippy::pedantic
 cargo test --all-targets --all-features
